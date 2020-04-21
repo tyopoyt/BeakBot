a0615fe6351132886acfcb5a11375a2a7e7e620b
tyopoyt <crousedb@dukes.jmu.edu>
2020-04-21 18:19:47 -0400

    Remove unversioned files

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,21 +2,27 @@ import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.Event;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-
+import discord4j.core.object.entity.MessageChannel;
+import reactor.core.publisher.Mono;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Bot {
 
   private static final Map<String, Command> commands = new HashMap<>();
   private static String prefix = "!", content = "";
+  private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
 
   static {
-    commands.put("ping", event -> event.getMessage()
-            .getChannel().block()
-            .createMessage("Pong!").block());
+    commands.put("ping", event -> Objects.requireNonNull(event.getMessage()
+            .getChannel().block()).createMessage("Pong!").block());
 
-    commands.put("prefix", event -> setPrefix(event));
+    commands.put("prefix ", Bot::setPrefix);
+
+    commands.put("localtime", Bot::tellTime);
   }
 
   /**
@@ -41,23 +47,34 @@ public class Bot {
     client.login().block();
   }
 
+
+  private static void sendMessage(Mono<MessageChannel> channel, String message) {
+    Objects.requireNonNull(channel.block()).createMessage(message).block();
+  }
+
   /**
    * Change the prefix for the bot.
    *
    * @param event the event triggering this call
    */
   private static void setPrefix(Event event) {
-
     try {
       prefix = content.substring(prefix.length() + "prefix ".length());
+      sendMessage(((MessageCreateEvent)event).getMessage().getChannel(),
+              "Prefix set to " + prefix);
 
-      ((MessageCreateEvent)event).getMessage()
-              .getChannel().block().createMessage("Prefix set to " + prefix).block();
+      System.out.println( "\\u" + Integer.toHexString(prefix.charAt(0) | 0x10000).substring(1) );
+      //System.out.printf("character: %d\n", (int)prefix.charAt(0));
 
     } catch (StringIndexOutOfBoundsException e) {
-      ((MessageCreateEvent)event).getMessage()
-              .getChannel().block().createMessage("Error setting prefix! Kept as: " + prefix).block();
+      sendMessage(((MessageCreateEvent)event).getMessage().getChannel(),
+              "Error setting prefix! Kept as: " + prefix);
     }
+  }
 
-    }
+  private static void tellTime(MessageCreateEvent event) {
+    sendMessage(event.getMessage().getChannel(),
+            "Bot's local time is: " + dtf.format(LocalDateTime.now()));
+  }
+
 }
